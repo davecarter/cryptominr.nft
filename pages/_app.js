133ac9cc268075514a014cb6bd8900599916f824
David Garcia <dave74@gmail.com>
2022-10-16 20:49:52 +0200

    create homePage and wrapp the providers

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import { Providers } from "components/context"
+import "../styles/globals.css"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Providers>
+        <Component {...pageProps} />
+      </Providers>
+      <div id="react-modal-portal" />
+    </>
+  )
 }
 
 export default MyApp
