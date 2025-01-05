ab65e54359aec8d8beeb1cdc6a08485569bd11d3
David G. <dave74@gmail.com>
2025-01-05 14:02:36 +0100

    adds DeleteAllBlocksUseCase

--- domain/blockchain/useCases/index.js
+++ domain/blockchain/useCases/index.js
@@ -1,3 +1,4 @@
 export { GetBlockUseCase } from "./GetBlockUseCase"
 export { AddNewBlockUseCase } from "./AddNewBlockUseCase"
 export { GetMinedBlockUseCase } from "./GetMinedBlockUseCase"
+export { DeleteAllBlocksUseCase } from "./DeleteAllBlocksUseCase"
