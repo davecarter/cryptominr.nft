a1ce5555c72497b586509f6384ca884c9f1fa697
David Garcia <dave74@gmail.com>
2025-01-09 19:50:53 +0100

    fix reloading with empty blocklist

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,19 @@
+import { useEffect } from "react"
 import { Providers } from "components/context"
+import globalStyles from "../styles/Globals.module.css"
 import "../styles/globals.css"
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    document.body.classList.add(globalStyles.globalReset)
+    document.documentElement.classList.add(globalStyles.globalResetAll)
+  }, [])
+
   return (
-    <>
-      <Providers>
-        <Component {...pageProps} />
-      </Providers>
+    <Providers>
+      <Component {...pageProps} />
       <div id="react-modal-portal" />
-    </>
+    </Providers>
   )
 }
 
