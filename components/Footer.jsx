7e17a85e0ac20a364b62067ba1b60a27d46e90f7
davecarter <dave74@gmail.com>
2025-02-21 17:32:28 +0100

    improve footer in mobile

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,17 +1,24 @@
-import styles from '../styles/Footer.module.css';
+import styles from "../styles/Footer.module.css"
 
 export const Footer = () => (
   <footer className={styles.footer}>
     <a href="https://github.com/davecarter/cryptominr.nft" target="_blank" rel="noopener noreferrer">
       Created by David Garcia - 2024
     </a>
-    <span> | </span>
-    <a
-      href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Powered by Vercel
-    </a>
+    <span className={styles.footerMeta}>
+      <span> | </span>
+    </span>
+    <span className={styles.footerMeta}>
+      <a
+        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Powered by Vercel
+      </a>
+      <small>
+        <a href="http://www.freepik.com">Images by Freepik</a>
+      </small>
+    </span>
   </footer>
 )
